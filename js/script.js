16c187ed8ea21f692ae85e8df99dda9c204effeb
Maksim Leskin <Maksim.leskin@methed.ru>
2023-07-20 13:19:34 +0300

    add modal cart

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ const getData = async () => {
 };
 
 const createCard = (item) => {
-  console.log("item: ", item);
   const cocktail = document.createElement("article");
   cocktail.classList.add("cocktail");
 
@@ -33,7 +32,49 @@ const createCard = (item) => {
   return cocktail;
 };
 
+const modalController = ({ modal, btnOpen, time = 300 }) => {
+  const buttonElem = document.querySelector(btnOpen);
+  const modalElem = document.querySelector(modal);
+
+  modalElem.style.cssText = `
+    display: flex;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity ${time}ms ease-in-out;
+  `;
+
+  const closeModal = (event) => {
+    const target = event.target;
+    const code = event.code;
+
+    if (target === modalElem || code === "Escape") {
+      modalElem.style.opacity = 0;
+
+      setTimeout(() => {
+        modalElem.style.visibility = "hidden";
+      }, time);
+
+      window.removeEventListener("keydown", closeModal);
+    }
+  };
+
+  const openModal = () => {
+    modalElem.style.visibility = "visible";
+    modalElem.style.opacity = 1;
+    window.addEventListener("keydown", closeModal);
+  };
+
+  buttonElem.addEventListener("click", openModal);
+  modalElem.addEventListener("click", closeModal);
+
+  return { openModal, closeModal };
+};
+
 const init = async () => {
+  modalController({
+    modal: ".modal_order",
+    btnOpen: ".header__btn-order",
+  });
   const goodsListElem = document.querySelector(".goods__list");
   const data = await getData();
 
